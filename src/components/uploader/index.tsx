536c7485b5e4da8bb679af4c0b90a171ddf29bd0
Caos <caosbad@gmail.com>
2021-02-27 11:53:09 +0800

    feat: add ipfs integration and display with uploader

--- src/components/uploader/index.tsx
+++ src/components/uploader/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { Input } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { Input, Image } from '@chakra-ui/react';
 
 const ipfsClient = require('ipfs-http-client');
 
 export default function Uploader() {
+  const [cid, setCid] = useState();
+
   const saveToIpfs = async (files = []) => {
     const ipfs = ipfsClient('http://59.110.115.146:5001');
     if (files.length === 0) {
@@ -14,6 +16,7 @@ export default function Uploader() {
         progress: (prog: any) => console.log(`received: ${prog}`),
       });
       console.log(added.cid.toString(), '=============');
+      setCid(added.cid.toString());
     } catch (err) {
       console.error(err);
     }
@@ -33,6 +36,7 @@ export default function Uploader() {
     <form id="capture-media" onSubmit={handleSubmit}>
       <Input type="file" onChange={captureFile} />
       <br />
+      {cid && <Image src={`http://59.110.115.146:8080/ipfs/${cid}`} />}
     </form>
   );
 }
