5c4a181abff49e4c5d45a3e7265525978dcbc0ec
lsz <1038747706@qq.com>
2021-03-02 23:06:39 +0800

    feat: explore page

--- src/pages/explore/MainList/index.js
+++ src/pages/explore/MainList/index.js
@@ -3,6 +3,8 @@ import { Box } from '@chakra-ui/react';
 import Collection from '../../../components/collection';
 
 const MainList = () => {
+  const typeList = [{ name: '全部', id: 0 }];
+
   const list = [
     { name: '星空', price: 1, id: 0 },
     { name: '星空', price: 1, id: 1 },
@@ -18,6 +20,19 @@ const MainList = () => {
 
   return (
     <Box width="1104px">
+      <Box
+        display="flex"
+        ml="16px"
+        mb="20px"
+        height="60px"
+        borderRadius="4px"
+        backgroundColor="#fff"
+        boxShadow="0px 2px 4px 0px rgba(0, 0, 0, 0.06)"
+      >
+        {typeList.map(({ id, name }) => (
+          <Box key={id}>{name}</Box>
+        ))}
+      </Box>
       <Box display="flex" flexWrap="wrap">
         {list.map(({ name, price, id }) => (
           <Box ml="16px" mb="16px">
