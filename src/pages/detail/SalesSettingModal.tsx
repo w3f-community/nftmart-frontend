a0789a6905186952562a94370006190c0f3e7629
amagi <amagiddmxh@qq.com>
2021-03-06 18:14:42 +0800

    feat: add forms of sales setting

--- src/pages/detail/SalesSettingModal.tsx
+++ src/pages/detail/SalesSettingModal.tsx
@@ -5,11 +5,19 @@ import {
   ModalContent,
   ModalHeader,
   ModalCloseButton,
-  Box,
   Button,
   ModalBody,
   ModalFooter,
+  Input,
+  InputGroup,
+  InputRightAddon,
+  Stack,
+  Checkbox,
+  Radio,
+  RadioGroup,
 } from '@chakra-ui/react';
+import NFormControl from '../../components/formControl';
+import colors from '../../themes/colors';
 
 export interface SalesSettingModalProps {
   open: boolean;
@@ -24,7 +32,37 @@ const SalesSettingModal: FC<SalesSettingModalProps> = ({ open, onConfirm, onClos
       <ModalContent>
         <ModalHeader>Modal Title</ModalHeader>
         <ModalCloseButton />
-        <ModalBody>content</ModalBody>
+        <ModalBody paddingBottom={6}>
+          <Stack spacing={8}>
+            <NFormControl title="price" subtitle="price.subtitle">
+              <InputGroup size="sm">
+                <Input placeholder="price" type="number" height="auto" />
+                <InputRightAddon children="suffix" height="auto" />
+              </InputGroup>
+            </NFormControl>
+            <NFormControl title="expiration" subtitle="expiration.subtitle">
+              <Checkbox color={colors.text.gray} defaultIsChecked>
+                expiration date
+              </Checkbox>
+            </NFormControl>
+            <NFormControl title="category" subtitle="category.subtitle" direction="vertical">
+              <RadioGroup color={colors.text.gray}>
+                <Stack direction="row" spacing={6}>
+                  <Radio value="1">First</Radio>
+                  <Radio value="2">Second</Radio>
+                  <Radio value="3">Third</Radio>
+                  <Radio value="4">Fourth</Radio>
+                </Stack>
+              </RadioGroup>
+            </NFormControl>
+            <NFormControl title="pledge" subtitle="pledge.subtitle">
+              <InputGroup size="sm">
+                <Input placeholder="price" type="number" height="auto" />
+                <InputRightAddon children="suffix" height="auto" />
+              </InputGroup>
+            </NFormControl>
+          </Stack>
+        </ModalBody>
 
         <ModalFooter display="flex" justifyContent="center">
           <Button variant="primary" onClick={onConfirm}>
