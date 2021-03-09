7394bb2cf992739e5511d15336dc45e384b7b467
amagi <amagiddmxh@qq.com>
2021-03-09 15:27:38 +0800

    feat(type): update asset type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,15 @@
 export interface Work {
+  id: number;
   Name: string;
   PicUrl?: string;
   Status: number;
-  id: number;
+  Address: string;
+  Describe: string;
+  Metadata: string;
+  ExternalLinks: string;
   price: number;
+  categoryId: number;
+  collectionId: number;
+  createdAt: string;
+  updatedAt: string;
 }
