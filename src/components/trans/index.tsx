a61949b95dcbe05184396c0e2597354a5bd29637
amagi <amagiddmxh@qq.com>
2021-03-18 05:44:04 +0800

    feat: remove spinner in trans provider

--- src/components/trans/index.tsx
+++ src/components/trans/index.tsx
@@ -1,6 +1,5 @@
 import React, { ReactNode, Suspense } from 'react';
 import { I18nextProvider } from 'react-i18next';
-import { Spinner } from '@chakra-ui/react';
 
 import i18n from '../../i18n';
 
@@ -11,7 +10,7 @@ type ContainerProps = {
 
 const Container = ({ children }: ContainerProps) => {
   return (
-    <Suspense fallback={<Spinner />}>
+    <Suspense fallback="...">
       <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
     </Suspense>
   );
